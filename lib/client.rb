450b0cc0870111c7dc993aca5cf67eeab060ba6a
= <alegrand@cinefil.com>
2015-04-08 14:41:29 +0200

    Exception handling

--- lib/client.rb
+++ lib/client.rb
@@ -6,8 +6,8 @@ Dir[File.join(File.dirname(__FILE__), 'wrappers', '*.rb')].each {|file| require 
 
 module Tmdby
   class Client
-    @@api_key     = ""
-    @@session_id  = ""
+    @@api_key     = nil
+    @@session_id  = nil
     @@api_url     = "http://api.themoviedb.org"
     @@api_version = 3
 
@@ -20,13 +20,15 @@ module Tmdby
         params[:api_key] = @@api_key
 
         uri = URI("#{@@api_url}/#{@@api_version}/#{api_route}?#{URI.encode_www_form(params)}")
-        puts uri
         response = Net::HTTP.get_response(uri)
 
-        Tmdby::Response.new(response)
-
+        if response.is_a? Net::HTTPSuccess
+          Tmdby::Response.new(response)
+        else
+          raise RuntimeError, "An error has occured : #{response.body}"
+        end
       else
-        raise RuntimeError, 'An error has occured : please specify TMDB API KEY' unless @@api_key
+        raise RuntimeError, 'An error has occured : please specify TMDB API KEY'
       end
     end
   end
