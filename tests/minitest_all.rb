3f98835ff5a075bb0b703b782ddcd73dec552e1f
= <alegrand@cinefil.com>
2015-04-22 11:42:39 +0200

    Tests done

--- tests/minitest_all.rb
+++ tests/minitest_all.rb
@@ -2,7 +2,7 @@ $:<< File.expand_path(File.dirname(__FILE__)) + '/..'
 
 require_relative 'credentials'
 
-if not (API_KEY and USERNAME and PASSWORD and ACCOUNT_ID)
+if not (API_KEY and USERNAME and PASSWORD and ACCOUNT_ID and API_KEY != "")
   credentials_file = File.expand_path('credentials.rb', File.dirname(__FILE__))
   $stderr.puts "Please configure your credentials in #{credentials_file}"
   exit
